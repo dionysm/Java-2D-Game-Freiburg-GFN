db1ad5390a7ca586c95ee491fcc846b419a6797d
Dejann <dejan.tankoski@hotmail.com>
2025-04-11 15:03:35 +0200

    window?

--- core/src/main/java/io/github/tesgame/Main.java
+++ core/src/main/java/io/github/tesgame/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
-    public void create() {
+    public void  create() {
         setScreen(new FirstScreen());
     }
 }
