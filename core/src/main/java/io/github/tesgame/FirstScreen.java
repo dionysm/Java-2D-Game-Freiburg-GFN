21ba519060bc9deb96e44c338d6ca9555df9f571
Dejann <dejan.tankoski@hotmail.com>
2025-04-14 08:56:03 +0200

    rotte punkt

--- core/src/main/java/io/github/tesgame/FirstScreen.java
+++ core/src/main/java/io/github/tesgame/FirstScreen.java
@@ -1,41 +1,49 @@
 package io.github.tesgame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-/** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    private ShapeRenderer shapeRenderer;
+
     @Override
     public void show() {
-        // Prepare your screen here.
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        float centerX = Gdx.graphics.getWidth() / 2f;
+        float centerY = Gdx.graphics.getHeight() / 2f;
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.circle(centerX, centerY, 10);
+        shapeRenderer.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        // Resize your screen here. The parameters represent the new window size.
+        // Resize logic (optional)
     }
 
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-        // This method is called when another screen replaces this one.
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+        shapeRenderer.dispose();
     }
 }
