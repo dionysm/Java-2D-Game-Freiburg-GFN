8f618fa83940c80b851677e70d2b2d2c12e2a6dc
dio-mb <dionysm@gmail.com>
2025-04-14 09:21:48 +0200

    new assets

---
+++ core/src/main/java/io/github/tesgame/Hero.java
@@ -0,0 +1,28 @@
+package io.github.tesgame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Hero {
+    private Sprite sprite;
+
+    public Hero() {
+        Texture texture = new Texture("walk.png"); // Bild im assets-Ordner
+        sprite = new Sprite(texture);
+        sprite.setPosition(100, 100); // Anfangsposition
+        sprite.setSize(64, 64); // Größe anpassen (optional)
+    }
+
+    public void update(float deltaTime) {
+        // Hier könntest du Bewegungslogik einbauen
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public void dispose() {
+        sprite.getTexture().dispose();
+    }
+}
